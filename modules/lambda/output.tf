bde905e527728937d39e7f35837ce494cc288a2d
Karthik-bn151193 <karthik.bn15111993@gmail.com>
2025-04-18 15:45:16 +0530

    Delete modules/lambda directory

--- modules/lambda/output.tf
+++
@@ -1,9 +0,0 @@
-output "arn" {
-  value       = aws_lambda_function.main.arn
-  description = "ARN of the Lambda function"
-}
-
-output "invoke_arn" {
-  value       = aws_lambda_function.main.invoke_arn
-  description = "Invoke ARN of the Lambda function"
-}
